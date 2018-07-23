8bc9fc6153647eea32f94634c0ecd0509ba8ed0c
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-07-24 00:55:33 +0530

    created a simple search handler

--- handlers/search.php
+++ handlers/search.php
@@ -2,5 +2,15 @@
 if (! defined('SHODANAPIKEY')) exit;
 
 function search(){
+    // Fetching query
+    $query = isset($_GET['query'])? $_GET['query']: '';
 
+    // Empty queries aren't allowed
+    if (empty($query)) redirect('index.php');
+
+    // Call Shodan API
+    $result = httpget('https://api.shodan.io/shodan/host/search?key='. urlencode(SHODANAPIKEY) .'&query=' . urlencode($query));
+
+    // Rendering result in view
+    var_dump($result);
 }
