1136ec0970bcd96e35a20a5030fd9181d7ed6728
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-07-23 23:39:33 +0530

    created route handlers

---
+++ handlers/home.php
@@ -0,0 +1,6 @@
+<?php
+if (! defined('SHODANAPI')) exit;
+
+function home(){
+
+}
--- index.php
+++ index.php
@@ -5,18 +5,22 @@ require_once 'apikey.php';
 // Initializing the Shodan client
 $client = new Shodan(SHODANAPIKEY, true);
 
+// Requiring route handlers
+require_once 'handlers/home.php';
+require_once 'handlers/search.php';
+require_once 'handlers/search.php';
+
 // What action is needed to be performed?
-$page = isset($_GET['page'])? $_GET['page']: 'home';
+$page = isset($_GET['page'])? strtolower(trim($_GET['page'])): 'home';
 
 // Routing requests according to $page
 switch($page){
     case 'home':
-
+        home();
     break;
     case 'search':
-
+        search();
     break;
     default:
-
+        notfound();
 }
-
