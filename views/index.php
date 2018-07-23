03e074a8f16b75da29626705b9a02d5e7c91c508
Aiden Luo <aiden@labex.io>
2018-07-23 23:41:58 +0530

    created basic homepage

--- views/index.php
+++ views/index.php
@@ -1,4 +1,18 @@
 <?php
-if (! defined('SHODANAPIKEY')) exit;
+//if (! defined('SHODANAPIKEY')) exit;
 ?>
 
+<html>
+<head>
+<title>Welcome to IoT Search</title>
+<link rel="stylesheet" href="views/style.css">
+</head>
+<body>
+    <form action="index.php" class="center">
+        <input type="hidden" name="page" value="search">
+        <input type="text" placeholder="e.g. search for ip">
+        <button type="submit">Search</button>
+    </form>
+    
+</body>
+</html>
