9df53283fbfe60c25a3fb69fd0a9ced7817d0ebd
Aiden Luo <aiden@labex.io>
2018-08-01 20:16:00 +0530

    made changes in the search results page

--- views/search.php
+++ views/search.php
@@ -11,51 +11,43 @@ if (! defined('SHODANAPIKEY')) exit;
     <link rel="stylesheet" href="views/style_s.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
 </head>
 <body>
-    <div class="cntainer">
-    <form action="index.php" method="get" class="form-inline">
-        <input type="hidden" name="page" value="search">
-        
-        <div class="col col-lg-3 col-md-3 col-sm-3 sss">
-            <input type="text" name="query" value="<?=htmlspecialchars($query)?>" style="border-color: #40B1EE" class="form-control">
+    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form action="" class="form-inline my-2 m-lg-0">
+                <input type="hidden" name="page" value="search">
+                <input class="form-control mr-sm-2" type="text" name="query" value="<?=htmlspecialchars($query)?>">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-        <div class="col col-lg-2 col-md-2 col-sm-2 sebtn">
-            <button type="submit" class="btn btn-info">Search</button>
-        </div>
-    </div>
+    </nav>
        
         
-    <hr>
 
     <?php foreach($results->matches as $result):?>
-    <div class="container ">
-        <div class="col col-lg-8 col-md-8 col-sm-8 ip">
-            <h2>
-                <a href="index.php?page=ip&amp;ip=<?=urlencode($result->ip_str)?>">
-                    <?=htmlspecialchars($result->ip_str)?>
-                </a>
-            </h2>
-        </div>
-        <div class="col col-lg-4 col-md-4 col-sm-4 search-results-summary">
-            <p id="isp">ISP: <?=htmlspecialchars($result->isp)?></p>
-            <h4>Port: <?=htmlspecialchars($result->port)?></h4>
 
-
-            <?php foreach($result->hostnames as $hostnames):?>
-            <div>
-            <p>Hostnames: <?=htmlspecialchars($result->hostnames[0])?></p>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        <a href="index.php?page=ip&amp;ip=<?=urlencode($result->ip_str)?>">
+                        <?=htmlspecialchars($result->ip_str)?>
+                        </a>
+                    </h2>
+                    <p id="isp">ISP: <?=htmlspecialchars($result->isp)?></p>
+                    <h4 id="port">Port: <?=htmlspecialchars($result->port)?></h4>
+                    <?php foreach($result->hostnames as $hostnames):?>
+                    <p id="hostnames">Hostnames: <?=htmlspecialchars($result->hostnames[0])?></p>
+                    <?php endforeach;?>
+                    <p id="location">Location: <?=htmlspecialchars($result->location->country_name)?></p>
+                    <p id="timestamp">Added: <?=htmlspecialchars($result->timestamp)?></p>
+                </div>
             </div>
-            <?php endforeach;?>
-            <p>Location: <?=htmlspecialchars($result->location->country_name)?></p>
-            <p>Added: <?=htmlspecialchars($result->timestamp)?></p>
         </div>
     </div>
-    
-        
-
-        
-        
     
 
 
