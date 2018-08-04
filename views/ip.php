9fff5659bbca93b1704c4a6ef535f360bd0d2eba
Aiden Luo <aiden@labex.io>
2018-08-04 11:35:51 +0530

    changed the ip page

--- views/ip.php
+++ views/ip.php
@@ -10,6 +10,9 @@ if (! defined('SHODANAPIKEY')) exit;
     <link rel="stylesheet" href="views/style_ip.css">
     <title>IP: <?=htmlspecialchars($ip->ip)?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
 </head>
 <body>
 
@@ -24,34 +27,57 @@ if (! defined('SHODANAPIKEY')) exit;
     </nav>
 
 
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <div class="host">
-                <div class="row-fluid">
-                    <div class="span6">
-                        <div class="page-header">
-                            <h1><?=htmlspecialchars($ip->ip_str)?></h1>
-                            <p><?=htmlspecialchars($ip->os)?></p>
-                            <p><?=htmlspecialchars($ip->region_code)?></p>
-                            <p><?=htmlspecialchars($ip->longitude)?></p>
-                            <h3><?=htmlspecialchars($ip->isp)?></h3>
-                            <h3><?=htmlspecialchars($ip->last_update)?></h3>
-                            <h2><?=htmlspecialchars($ip->asn)?></h2>
 
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="row ikl">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+            <h1 class="ip_header"><i class="fa fa-globe"></i>  <?=htmlspecialchars($ip->ip_str)?></h1>
 
-                    <div class="span6">
-                        <div class="page-header">
-                            <h2>Ports:</h2>
-                            <ul>
-                            <?php foreach($ip->ports as $port):?>
-                                <li><?=htmlspecialchars($port)?></li>
-                            <?php endforeach;?>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+            <hr>
+
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>City</td>
+                        <th><?=htmlspecialchars($ip->city)?></th>
+                    </tr>
+                    <tr>
+                        <td>Country</td>
+                        <th><?=htmlspecialchars($ip->country_name)?></th>
+                    </tr>
+                    <tr>
+                        <td>Organisation</td>
+                        <th><?=htmlspecialchars($ip->org)?></th>
+                    </tr>
+                    <tr>
+                        <td>ISP</td>
+                        <th><?=htmlspecialchars($ip->isp)?></th>
+                    </tr>
+                    <tr>
+                        <td>Last Update</td>
+                        <th><?=htmlspecialchars($ip->last_update)?></th>
+                    </tr>
+                    <tr>
+                        <td>ASN</td>
+                        <th><?=htmlspecialchars($ip->asn)?></th>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+            <h1 class="ip_header"><i class="fa fa-server"></i>  Ports</h1>
+            <hr>
+            <ul class="ports">
+                <?php foreach($ip->ports as $port):?>
+                    <li><?=htmlspecialchars($port)?></li>
+                <?php endforeach;?>
+            </ul>
+
+            <h1 class="ip_header"><i class="fa fa-reorder"></i>  Services</h1>
+            <hr>
+
+
+
             </div>
         </div>
     </div>
