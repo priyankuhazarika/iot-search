9f611993b2c3fb2b37b620e7728513d2839386ed
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-08-09 23:50:14 +0530

    Added github repo link in footer

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 
 
     <div class="container">
-        <div id="footer">&copy;Powered by Shodan</div>
+        <div id="footer">Powered by Shodan | <a href="https://github.com/Priyankuabi/iot-search">GitHub</a></div>
     </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
